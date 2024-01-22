6ff4320a1ca4bcb87da270c0b49b265250af32ec
Otmane Boujlam <otmane.boujlam@gmail.com>
2024-01-22 17:37:01 +0100

    commander

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,6 +4,18 @@ function Card({ pizza }) {
       <img src={pizza.image} className="card-img-top" alt="" />
       <div className="card-body">
         <h5 className="card-title">{pizza.name}</h5>
+        <ul>
+          {pizza.ingredients.map((i) => (
+            <li key={i}>{i}</li>
+          ))}
+        </ul>
+        <button
+          onClick={() => window.alert(`Vous avez commandé : ${pizza.name}`)}
+          type="button"
+          className=" btn btn-sm btn-outline-info"
+        >
+          Commander
+        </button>
       </div>
     </div>
   );
